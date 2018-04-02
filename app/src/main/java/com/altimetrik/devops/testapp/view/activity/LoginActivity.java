922f2b19e0f3833f135ddc268b0f9c450b53a9af
Abhijeet Bhosale <abhijeet.bhosale@altimetrik.com>
2018-04-02 12:41:14 +0530

    Initial commit

---
+++ app/src/main/java/com/altimetrik/devops/testapp/view/activity/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.altimetrik.devops.testapp.view.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.altimetrik.devops.testapp.R;
+
+/**
+ * Created by abhosale on 4/2/2018.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
